70d096619e5ebbb847419857f344fb1b1f4d24ec
albuquerque.r.lucas <albuquerque.r.lucas@gmail.com>
2016-05-14 08:18:28 -0300

    Adicionando o projeto...

---
+++ application/views/myminisweets/cadastro.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="utf-8" />
+        <title>Login</title>
+
+    </head>
+    <body>
+        <h1 align="center">Página de Cadastro</h1><hr/>
+        <br/>
+        <a href="<?php echo base_url('welcome'); ?>">voltar...</a>
+
+
+    </body>
+</html>
---
+++ application/views/myminisweets/login.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="utf-8" />
+        <title>Login</title>
+
+    </head>
+    <body>
+        <h1 align="center">Página de Login</h1><hr/>
+        <br/>
+        <a href="<?php echo base_url('welcome'); ?>">voltar...</a>
+
+    </body>
+</html>
